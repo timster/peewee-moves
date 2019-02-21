060310ec004a234b4a979831ee3198d83c5ea5fd
Timothy Shaffer <tshaffer@mitre.org>
2019-02-21 11:01:02 -0500

    Bump version: 2.0.0 → 2.0.1

--- peewee_moves.py
+++ peewee_moves.py
@@ -12,7 +12,7 @@ import peewee
 from playhouse.db_url import connect as url_connect
 from playhouse.migrate import SchemaMigrator
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 __all__ = ['DatabaseManager', 'MigrationHistory', 'Migrator', 'SchemaMigrator', 'TableCreator']
 
